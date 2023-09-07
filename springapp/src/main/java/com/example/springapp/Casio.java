9762882903756f6c7769836ef0b65628b110edfb
superdemo09 <superdemo72@gmail.com>
2023-09-07 07:41:46 +0000

    commit 77

--- springapp/src/main/java/com/example/springapp/Casio.java
+++ springapp/src/main/java/com/example/springapp/Casio.java
@@ -21,9 +21,6 @@ public class Casio implements Calculator{
         diff=num2-num1;
         // System.out.println(num2-num1);      
           return diff;
-          //welcome
-          //welcome1
-          //welcome2
-          //welcome4
+          console.log("hai")
     }
 }
