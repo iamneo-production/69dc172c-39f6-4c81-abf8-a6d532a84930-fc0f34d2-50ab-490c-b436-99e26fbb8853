7eb53469e985f23314c19551bbc432e775ed2de0
superdemo42 <superdemo46@gmail.com>
2023-09-07 08:27:51 +0000

    s

--- springapp/src/main/java/com/example/springapp/Casio.java
+++ springapp/src/main/java/com/example/springapp/Casio.java
@@ -3,7 +3,11 @@ public class Casio implements Calculator{
    
     int num1,num2;
     public void setNum1(int num1) {
-        this.num1 = num1;
+        this.num
+        
+        
+        
+        1 = num1;
     }
     public void setNum2(int num2) {
         this.num2 = num2;
